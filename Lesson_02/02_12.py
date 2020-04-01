cea6772c454362e78b9f555ec73da05e203154d6
0xfaust <robert.mcconnell@protonmail.com>
2020-04-01 20:57:42 +0100

    modified Lesson 02, 12-13

--- Lesson_02/02_12.py
+++ Lesson_02/02_12.py
@@ -11,14 +11,14 @@ expected?
 '''
 
 def all_the_snacks(snack, spacer):
-    return ((snack + (' '  * spacer)) * 100)
+    return ((snack + spacer) * 100)
 
 shopping_list = {'milk':9.42, 'eggs':5.67, 'bread':3.25, 'apples':13.40, 'tea':7.50}
 
 for item in shopping_list:
-    print(all_the_snacks(item, 2))
+    print(all_the_snacks(item, ', '))
 
-#print(all_the_snacks('hello', 'world'))
+print(all_the_snacks('hello', ' '))
 #print(all_the_snacks(5,3))
-print(all_the_snacks('hello', 5))
+#print(all_the_snacks('hello', 5))
 #print(all_the_snacks(5, 'hello'))
--- Lesson_02/02_13.py
+++ Lesson_02/02_13.py
@@ -7,10 +7,10 @@ customise the number of times your snack gets printed out.
 '''
 
 def all_the_snacks(snack, spacer, num):
-    return ((snack + (' '  * spacer)) * num)
+    return ((snack + spacer) * num)
 
 shopping_list = {'milk':9.42, 'eggs':5.67, 'bread':3.25, 'apples':13.40, 'tea':7.50}
 
 for item in shopping_list:
-    print(all_the_snacks(item, 2, 3))
+    print(all_the_snacks(item, ', ', 3))
 
